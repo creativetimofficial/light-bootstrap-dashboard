63356144924073ca45f1bb523cd80b18ad7411f6
Stefan Moldovan <moldovanste@gmail.com>
2019-07-08 11:57:21 +0300

    Update Product Copyright

--- assets/js/light-bootstrap-dashboard.js
+++ assets/js/light-bootstrap-dashboard.js
@@ -1,18 +1,16 @@
-/*!
-
- =========================================================
- * Light Bootstrap Dashboard - v2.0.1
- =========================================================
-
- * Product Page: http://www.creative-tim.com/product/light-bootstrap-dashboard
- * Copyright 2017 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
+// =========================================================
+//  Light Bootstrap Dashboard - v2.0.1
+// =========================================================
+//
+//  Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard
+//  Copyright 2019 Creative Tim (https://www.creative-tim.com)
+//  Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE)
+//
+//  Coded by Creative Tim
+//
+// =========================================================
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 var searchVisible = 0;
 var transparent = true;
